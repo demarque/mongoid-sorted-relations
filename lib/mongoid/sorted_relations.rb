7d8406cbab2e2d281fed71391bc3ee7526c7d140
Sebastien Rosa <sebastien@demarque.com>
2012-05-15 14:36:26 -0400

    Cache the sorted documents

--- lib/mongoid/sorted_relations.rb
+++ lib/mongoid/sorted_relations.rb
@@ -4,24 +4,35 @@ module Mongoid
 
     def self.included(base)
       base.extend(ClassMethods)
+      base.after_initialize :freeze_relation_ids
     end
 
     module ClassMethods
       def init_sorted_relations
         self.relations.each do |k,v|
-          if [:references_many, :references_and_referenced_in_many].include? v.macro
-            send(:define_method, "sorted_#{v.name}") { sorted_relation v }
-          end
+          send(:define_method, "sorted_#{v.name}") { sorted_relation v } if [:references_many, :references_and_referenced_in_many].include? v.macro
+        end
+      end
+    end
+
+    def freeze_relation_ids
+      @cache_sorted_documents = {}
+      @cache_sorted_ids = {}
+
+      self.relations.each do |k,v|
+        if [:references_many, :references_and_referenced_in_many].include? v.macro
+          @cache_sorted_ids[v.key] = [self.send(v.key)].flatten.map{ |rid| rid.to_s }
         end
       end
     end
 
     def sorted_relation(relation)
-      ids = self.send(relation.key).map{ |rid| rid.to_s }
+      if not @cache_sorted_documents[relation.name]
+        documents = self.send(relation.name).sort_by { |x| @cache_sorted_ids[relation.key].index(x.id.to_s) }
+        @cache_sorted_documents[relation.name] = Mongoid::FakeCriteria.new(documents)
+      end
 
-      documents = self.send(relation.name).sort_by { |x| ids.index(x.id.to_s) }
-
-      return Mongoid::FakeCriteria.new(documents)
+      return @cache_sorted_documents[relation.name]
     end
   end
 end
